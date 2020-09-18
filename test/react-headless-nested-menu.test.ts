f4a8ad661c066a1dcefe8958a38dc5390489e5bd
Ahmad Al Haddad <haddad@ah.sa>
2020-09-18 18:50:41 +0300

    Edit dummy test

--- test/react-headless-nested-menu.test.ts
+++ test/react-headless-nested-menu.test.ts
@@ -1,14 +1,5 @@
-import DummyClass from '../src/react-headless-nested-menu'
-
-/**
- * Dummy test
- */
 describe('Dummy test', () => {
-  it('works if true is truthy', () => {
+  it('Works', () => {
     expect(true).toBeTruthy()
-  })
-
-  it('DummyClass is instantiable', () => {
-    expect(new DummyClass()).toBeInstanceOf(DummyClass)
   })
 })
